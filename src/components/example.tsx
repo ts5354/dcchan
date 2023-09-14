ded77c713986ccdcb868897bdcf41c6fcd9c9b1a
sallykaityo <tsunodashuma@gmail.com>
2023-09-14 22:10:29 +0900

    新規

---
+++ src/components/example.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const example = () => {
+  return (
+    <div>example</div>
+  )
+}
---
+++ src/components/infomation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const infomation = () => {
+  return (
+    <div>
+        <p className="what">どのページに行きますか？</p>
+    </div>
+  )
+}
